ea58ec884e49139e8cf48f01131fa50b72b44d03
Suraj Kalekar <55831264+surajkarlekar@users.noreply.github.com>
2024-03-19 20:35:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,37 +1,44 @@
-require('dotenv').config();
-const express = require('express');
-const cors = require('cors');
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
 const app = express();
-const bodyParser = require("body-parser")
+const bodyParser = require("body-parser");
+const validUrl = require("valid-url");
 
 // To mount the bodyParser middleware in root level which will be called for all routes
-app.use((bodyParser.urlencoded({extended: false})) );
+app.use(bodyParser.urlencoded({ extended: false }));
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
 
-app.use('/public', express.static(`${process.cwd()}/public`));
+app.use("/public", express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
-  res.sendFile(process.cwd() + '/views/index.html');
+app.get("/", function (req, res) {
+  res.sendFile(process.cwd() + "/views/index.html");
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
-  res.json({ greeting: 'hello API' });
+app.get("/api/hello", function (req, res) {
+  res.json({ greeting: "hello API" });
 });
 
-app.get('/api/shorturl', function(req, res) {
-  res.json({ original_url : 'https://freeCodeCamp.org', short_url : 1});
+app.get("/api/shorturl", function (req, res) {
+  res.json({ original_url: "https://freeCodeCamp.org", short_url: 1 });
 });
 
-app.post('/api/shorturl', function(req, res) {
-  let url = req.body.url
-  res.json({ original_url : url, short_url : 123456});
+app.post("/api/shorturl", function (req, res) {
+  let url = req.body.url;
+
+  if (validUrl.isUri(url)) {
+    // ... your URL shortening logic here ...
+    res.json({ original_url: url, short_url: 123456 });
+  } else {
+    res.json({ error: "invalid url" });
+  }
 });
 
-app.listen(port, function() {
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
